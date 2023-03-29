f8f4fe0f9d420a08dc0d587b67d3da3f5995d048
Vadim-kokorev <kokorev-vadim@rambler.ru>
2023-03-29 23:43:48 +0300

    update a model of a house and create comment of code for the purpose of OOP practice

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-
-class Flat{
+/*объекты типа Flat являют собой построенные квартиры в доме, которые 
+обладают такими свойствами, как номер квартиры и комнаты в виде массива*/
+class Flat{                              
     public $number;
     public $rooms;
     function __construct($number,$room){
         $this->number = $number;  
         $this->rooms = $room; 
     }
+    /* следующие методы позволяют узнать номер квартиры, 
+    заменить его или создать, добавить квартиру в дом
+    или получить информацию о ее свойствах соответственно*/
     public function getNumber(){
         return $this->number;
     }
@@ -17,9 +21,9 @@ class Flat{
         array_push($this->rooms,$nextroom);
     }
     public function getRooms(){
-        echo $this->number;
+        echo " в квартире номер $this->number есть ";
         foreach ($this->rooms as $roomp){
-            echo $roomp->name;
+            echo  $roomp->name;
             echo " {$roomp->color} цвета";
             echo " длиной {$roomp->length}"; 
             echo " и шириной {$roomp->width}"; 
@@ -27,7 +31,8 @@ class Flat{
 }
 };
 $rooms = array();
-
+/* объекты типа Room-это ни что иное, как комнаты в квартирах, 
+которые имеют название, цвет и длину с шириной*/
 class Room{
     public $name;
     public $color;
@@ -39,16 +44,15 @@ class Room{
         $this->length = $length;
         $this->width = $width;
     }
-    function getInfo(){
-        return "{$this->name} "."{$this->color} цвета "."длиной {$this->length} "."и шириной {$this->width}";
-    }
-    public function checkColor(){
+    public function checkColor(){  //позволяет узнать цвет комнаты
         return $this->color;
     }
-    public function changeColor($color){
+    public function changeColor($color){   //позволяет поменять цвет комнаты
         $this->color = $color;
     }
 };
+/* объекты типа House - это многоквартирный дом, имеющий 
+адресс и информацию о квартирах, которе в нем построены*/
 class House{
     public $sity;
     public $street;
@@ -60,6 +64,8 @@ class House{
         $this->number = $number;
         $this->flats = $flat;
     }
+    /* методы класса House позволяют получить адрес дома, его номер или его добавление, 
+    а также добавить новую квартиру в дом и получить полную информацию о нем*/
     function getInfo(){
      echo "Дом находится в городе {$this->sity}, "."на улице {$this->street}"; 
     }
@@ -73,20 +79,37 @@ class House{
         array_push($this->flats,$nextflat);
     }
     public function getFlats(){
-        echo $this->number;
-        /*print_r($this->flats);*/
-        foreach ($this->flats as $key => $value){
-            echo "{$key} => {$value}";
+        echo " в доме номер $this->number есть квартиры с номерами: ";
+        foreach ($this->flats as $flatp){
+            echo "$flatp->number, ";
         }
+    }
+    public function getAllInfo($housep){
+        $housep->getInfo();
+        echo " под номером $this->number. Дом содержит в себе ";
+        foreach ($this->flats as $flatp){
+            echo "квартиру номер $flatp->number, в которой есть ";
+            foreach ($flatp->rooms as $roomp){
+                echo  $roomp->name;
+                echo " {$roomp->color} цвета,";
+                echo " длиной {$roomp->length}"; 
+                echo " и шириной {$roomp->width}; ";
+    }
+}
     }
 };
 $flats = array();
-$house1 = new House("Москва","Голубинская","15",$flats);
+$house1 = new House("Москва","Голубинская","15",$flats); 
+/*создаем объект типа House, с адрессом Москва, Улица Голубинская, 
+д.15. с пустым массивом квартир, потому что они еще не построены.*/
 $house1->addflats($flat1 = new Flat(NULL, $rooms));
 $house1->addflats($flat2 = new Flat("2", $rooms));
+/*созданы две разные квартиры в доме house1*/
 $house1->setNumber("123");
 $flat1->setNumber("15");
-$flat1->addRooms($room1 = new Room("Кухня","Белого","4м","3м"));
-$flat1->addRooms($room2 = new Room("Спальня","Синего","4м","3м"));
-$flat2->addRooms($room3 = new Room("Кухня","Желтого","5м","3м"));
-$flat2->getRooms();
+$flat1->addRooms($room1 = new Room("коридор","белого","4м","3м"));
+$flat1->addRooms($room2 = new Room("спальню","синего","4м","3м"));
+$flat2->addRooms($room3 = new Room("кухня","желтого","5м","3м"));
+/*построены комнаты в двух квартирах*/
+$house1->getAllInfo($house1); // используя метод getAllInfo класса House, получаем всю информацию о доме house1
+
